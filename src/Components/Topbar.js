07d47354a5ebcee648010708b655176a52c34241
Sahil Sutaria <saahil.sutaria@gmail.com>
2020-09-20 18:12:33 -0700

    updated images

--- src/Components/Topbar.js
+++ src/Components/Topbar.js
@@ -9,7 +9,7 @@ function Topbar() {
     <div className="top-bar">
         <Card style={{ width: '100vw' }}>
             <Card.Body style={{paddingLeft: "40px"}}>
-                <img width = "15%" height="30%" alt="stack-icon" src='https://image4.owler.com/logo/stackline_owler_20170129_010716_original.png'/>
+                <img width = "15%" height="30%" alt="stack-icon" src='https://github.com/saahil-sutaria/StacklineReactRedux/blob/master/public/stackline.png?raw=true'/>
             </Card.Body>
         </Card>
     </div>
